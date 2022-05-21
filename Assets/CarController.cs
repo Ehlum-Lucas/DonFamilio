c6a4639edb55df9659eaa0727a5aaba9af76c83d
Lisa Garnier <lisa.garnier@epitech.eu>
2022-05-21 13:41:07 +0200

    car movement

---
+++ Assets/CarController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public GameObject player;
+    public float speed = 1;
+    public bool move;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Vertical") && Input.GetAxisRaw("Vertical") > 0) {
+            player.transform.position = new Vector2(0.01f, -1.6f);
+    } 
+ else if (Input.GetButtonDown("Vertical") && Input.GetAxisRaw("Vertical") < 0) {
+            player.transform.position = new Vector2(0.01f, -2.7f);
+    }
+    }
+}
