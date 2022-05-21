e8146f30daf00fb7d91d5da0a849fbd5b25227ac
Ehlum-Lucas <lucas.iglesia@epitech.eu>
2022-05-21 11:20:40 +0200

    feat (all): add enemy car

---
+++ Assets/RandomGen.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomGen : MonoBehaviour
+{
+    public GameObject car;
+    private int nb = 0;
+    public float speed = 0.1f;
+
+    void init_car()
+    {
+        nb = Random.Range(0, 2);
+        Debug.Log(nb);
+        if (nb == 1)
+            car.transform.position = new Vector2(7.2f, -2.7f);
+        else
+            car.transform.position = new Vector2(7.2f, -1.6f);
+    }
+
+    void Start()
+    {
+        init_car();
+    }
+
+    void Update()
+    {
+        car.transform.position = car.transform.position + new Vector3(-speed, 0, 0);
+    }
+}
