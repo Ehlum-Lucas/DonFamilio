7f665c883cb5de9a359ee38c2913b6bc00dc89cb
Ehlum-Lucas <lucas.iglesia@epitech.eu>
2022-05-22 18:56:50 +0200

    fix (all): fix perspective

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -8,10 +8,21 @@ public class CarController : MonoBehaviour
     public float speed = 1;
     public bool move = false;
     public int pos;
+    public SpriteRenderer rend;
 
     void Start()
     {
         player.transform.position = new Vector2(-6, -2.7f);
+    }
+
+    void order()
+    {
+        if (player.transform.position.y == -2.7f)
+            rend.sortingOrder = 6;
+        else if (player.transform.position.y == -1.6f)
+            rend.sortingOrder = 3;
+        else
+            rend.sortingOrder = 0;
     }
 
     void Update()
@@ -46,6 +57,7 @@ public class CarController : MonoBehaviour
             }
             pos = 2;
         }
+        order();
     }
 }
 // < 0 = flech du bas hihi
